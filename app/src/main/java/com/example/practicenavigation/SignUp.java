6791acdee66900b6efcdaf257bb8051f721b8174
Sohailhamza <hafizsohail582@gmail.com>
2025-11-19 14:37:17 +0500

    V1 SignUp changes 19 Nov 2:36PM

--- app/src/main/java/com/example/practicenavigation/SignUp.java
+++ app/src/main/java/com/example/practicenavigation/SignUp.java
@@ -2,6 +2,8 @@ package com.example.practicenavigation;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,6 +13,16 @@ public class SignUp extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        Spinner spinner = findViewById(R.id.spinnerRole);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.roles,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
 
         TextView toLogin = findViewById(R.id.toLogin);
         toLogin.setOnClickListener(v -> {
